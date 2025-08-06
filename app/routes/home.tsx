b9218c9c581f1e927f56fd2faf0945e2336c0e26
Vaibhav Baliyan <vaibhavbaliyan2002@gmail.com>
2025-08-06 21:20:48 +0530

    Updated

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,29 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumize | Smart Resume Analysis" },
+    {
+      name: "description",
+      content: "Optimize your resume with AI-powered analysis and insights",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url(`/images/bg-home.svg`)] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications and Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+      </section>
+
+      {}
+    </main>
+  );
 }
